601adc50c3c1937c6a65b7602d5dfb00dfc5786c
pradhannikita-cs <pradhannikita555@gmail.com>
2026-07-15 14:04:41 +0530

    updated wesite pages and dashborad

--- db.py
+++ db.py
@@ -2,10 +2,17 @@ import mysql.connector
 import os
 
 def get_connection():
+
     return mysql.connector.connect(
-        host=os.environ.get("MYSQLHOST"),
-        user=os.environ.get("MYSQLUSER"),
-        password=os.environ.get("MYSQLPASSWORD"),
-        database=os.environ.get("MYSQLDATABASE"),
-        port=int(os.environ.get("MYSQLPORT"))
+
+        host=os.environ.get("MYSQLHOST", "localhost"),
+
+        user=os.environ.get("MYSQLUSER", "root"),
+
+        password=os.environ.get("MYSQLPASSWORD", ""),
+
+        database=os.environ.get("MYSQLDATABASE", "sentiment_db"),
+
+        port=int(os.environ.get("MYSQLPORT", 3306))
+
     )
